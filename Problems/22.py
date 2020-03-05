52ff720838f0cbba2fbdba3f5d3da55aa5bf2d5c
DavidAHazra <david.ogden493@gmail.com>
2020-03-05 08:27:39 +0000

    Moved data to ProblemData dir

--- Problems/22.py
+++ Problems/22.py
@@ -1,5 +1,6 @@
 import string
+import os
 
-with open("22_names.txt", 'r') as name_file:
+with open(os.path.join(os.getcwd(), "..", "ProblemData", "22_names.txt"), 'r') as name_file:
     print(sum((index + 1) * sum(string.ascii_uppercase.index(letter) + 1 for letter in name)
               for index, name in enumerate(sorted([name[1:-1] for name in name_file.read().split(',')]))))
